89a63570ea097a6924d12e6c5cf5b0481580b746
devGW <doscmDev@gmail.com>
2019-10-18 21:22:20 +0900

    feat: home view

--- screens/Home/HomeContainer.js
+++ screens/Home/HomeContainer.js
@@ -1,17 +1,58 @@
 import React, { Component } from "react";
 import { View, Text } from "react-native";
 import HomePresenter from "./HomePresenter";
+import styled from "styled-components";
+import { LIGTH_GREEN } from "../../constants/Color";
+import NavButton from "../../components/NavButton";
 
+const Image = styled.Image`
+  height: 70px;
+  width: 70px;
+`;
+const Title = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+  color: ${LIGTH_GREEN};
+`;
 class HomeContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
 
+  static navigationOptions = ({ navigation }) => ({
+    headerLeft: (
+      <>
+        <Image
+          source={require("../../assets/images/logo.png")}
+          resizeMode={"contain"}
+        />
+        <Title>학식이</Title>
+      </>
+    ),
+    headerRight: (
+      <>
+        <NavButton
+          iconName={"ios-search"}
+          color={LIGTH_GREEN}
+          onPress={() => navigation.navigate("Search")}
+        />
+        <NavButton
+          iconName={"ios-person"}
+          color={LIGTH_GREEN}
+          onPress={() => navigation.navigate("Profile")}
+        />
+      </>
+    )
+  });
+
   render() {
+    const {
+      navigation: { navigate }
+    } = this.props;
     return (
       <View>
-        <HomePresenter {...this.props} />
+        <HomePresenter navigate={navigate} />
       </View>
     );
   }
--- screens/Home/HomePresenter.js
+++ screens/Home/HomePresenter.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Text, View } from "react-native";
+import PropTypes from "prop-types";
 
-const HomePresenter = props => (
+const HomePresenter = ({ navigate }) => (
   <View>
-    <Text onPress={() => props.navigation.navigate("Posts")}>Feed</Text>
+    <Text onPress={() => navigate("Posts")}>Feed</Text>
   </View>
 );
+
+HomePresenter.propTypes = {
+  navigate: PropTypes.func.isRequired
+};
 
 export default HomePresenter;
