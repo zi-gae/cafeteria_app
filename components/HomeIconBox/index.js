f2176a9b0148eb0657c54395af7c0e305cb494bb
devGW <doscmDev@gmail.com>
2019-10-24 03:40:26 +0900

    feat: home icon component

---
+++ components/HomeIconBox/index.js
@@ -0,0 +1,49 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import { Ionicons, FontAwesome } from "@expo/vector-icons";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Linking } from "react-native";
+
+const IconBox = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
+const Touch = styled.TouchableOpacity`
+  align-items: center;
+  justify-content: center;
+  width: ${RFValue(40)};
+  height: ${RFValue(40)};
+`;
+const Text = styled.Text`
+  font-size: ${RFValue(9)};
+`;
+const View = styled.View`
+  background-color: #f2f2f2;
+  border-radius: 20px;
+`;
+
+const index = ({ name, size, color, type, kind, url }) => (
+  <IconBox>
+    <View>
+      <Touch onPress={() => Linking.openURL(url)}>
+        {type === "Ii" ? (
+          <Ionicons name={name} size={RFValue(size)} color={color} />
+        ) : (
+          <FontAwesome name={name} size={RFValue(size)} color={color} />
+        )}
+      </Touch>
+    </View>
+    <Text>{kind}</Text>
+  </IconBox>
+);
+
+index.propTypes = {
+  name: PropTypes.string.isRequired,
+  size: PropTypes.number.isRequired,
+  color: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  kind: PropTypes.string.isRequired
+};
+
+export default index;
