0447aea0feefa283b50285217fbbf45c6bf16468
devGW <doscmDev@gmail.com>
2019-10-28 18:02:07 +0900

    feat: write post style

--- components/WritePost/WritePostContainer.js
+++ components/WritePost/WritePostContainer.js
@@ -1,14 +1,82 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 import WritePostPresenter from "./WritePostPresenter";
+import styled from "styled-components";
+import { LIGTH_GREEN } from "../../constants/Color";
+import NavButton from "../NavButton";
+import { RFValue } from "react-native-responsive-fontsize";
 
-class WritePostContainer extends PureComponent {
+const Title = styled.Text`
+  font-weight: bold;
+  font-size: 18px;
+`;
+
+const Button = styled.TouchableOpacity`
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+  margin-right: ${RFValue(15)};
+`;
+const ButtonBox = styled.View`
+  background-color: ${LIGTH_GREEN};
+  justify-content: center;
+  align-items: center;
+  width: ${RFValue(60)};
+  height: ${RFValue(25)};
+  border-radius: ${RFValue(15)};
+`;
+const ButtonText = styled.Text`
+  font-size: ${RFValue(12)};
+  color: white;
+  font-weight: 600;
+`;
+
+class WritePostContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      isChecked: true
+    };
   }
 
+  static navigationOptions = ({ navigation }) => ({
+    tabBarVisible: false,
+    headerTitle: <Title>글 쓰기</Title>,
+    headerLeft: (
+      <NavButton
+        iconName="ios-close"
+        color={LIGTH_GREEN}
+        onPress={() => navigation.goBack(null)}
+      />
+    ),
+    headerRight: (
+      <Button>
+        <ButtonBox>
+          <ButtonText>완료</ButtonText>
+        </ButtonBox>
+      </Button>
+    )
+  });
+
+  handleCheckBox = () => {
+    const { isChecked } = this.state;
+    isChecked
+      ? this.setState({
+          isChecked: false
+        })
+      : this.setState({
+          isChecked: true
+        });
+  };
+
   render() {
-    return <WritePostPresenter />;
+    const { handleCheckBox } = this;
+    const { isChecked } = this.state;
+    return (
+      <WritePostPresenter
+        handleCheckBox={handleCheckBox}
+        isChecked={isChecked}
+      />
+    );
   }
 }
 
--- components/WritePost/WritePostPresenter.js
+++ components/WritePost/WritePostPresenter.js
@@ -1,10 +1,90 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { KeyboardAccessoryView } from "react-native-keyboard-accessory";
+import styled from "styled-components";
+import { RFValue } from "react-native-responsive-fontsize";
+import { BODER_COLOR, LIGTH_GREEN } from "../../constants/Color";
+import { Ionicons } from "@expo/vector-icons";
+import CheckBox from "react-native-check-box";
+import Layout from "../../constants/Layout";
 
-const WritePostPresenter = ({ params }) => (
-  <View>
-    <Text>WritePostPresenter</Text>
-  </View>
-);
+const Container = styled.View`
+  flex: 1;
+  padding-left: ${Layout.width / 20};
+  padding-right: ${Layout.width / 20};
+`;
+const ScrollView = styled.ScrollView``;
+
+const TitleInput = styled.TextInput`
+  font-size: ${RFValue(20)};
+  margin-top: ${RFValue(10)};
+  margin-bottom: ${RFValue(10)};
+  padding-bottom: ${RFValue(10)};
+  border-bottom-width: 1px;
+  border-bottom-color: ${BODER_COLOR};
+  font-weight: bold;
+`;
+const ContentInput = styled.TextInput`
+  font-size: ${RFValue(15)};
+  font-weight: 500;
+`;
+
+const ButtonLabel = styled.View`
+  height: ${RFValue(40)};
+  padding: 5px;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  background-color: white;
+`;
+const AnonymousBox = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+const Anonymous = styled.Text`
+  font-weight: 600;
+  font-size: ${RFValue(13)};
+  color: ${LIGTH_GREEN};
+`;
+const Touch = styled.TouchableOpacity``;
+
+const WritePostPresenter = ({ isChecked, handleCheckBox }) => {
+  return (
+    <Container>
+      <ScrollView>
+        <TitleInput
+          autoCapitalize="none"
+          autoCompleteType="off"
+          autoCorrect={false}
+          placeholder="제목"
+        />
+        <ContentInput
+          autoCapitalize="none"
+          autoCompleteType="off"
+          autoCorrect={false}
+          numberOfLines={10}
+          placeholder="제목"
+          placeholder="이곳에 글을 작성해 주세요"
+          multiline={true}
+        />
+      </ScrollView>
+      <KeyboardAccessoryView alwaysVisible={true}>
+        <ButtonLabel>
+          <Touch>
+            <Ionicons name="ios-camera" size={30} color={LIGTH_GREEN} />
+          </Touch>
+          <AnonymousBox>
+            <CheckBox
+              isChecked={isChecked}
+              onClick={handleCheckBox}
+              checkBoxColor={LIGTH_GREEN}
+            />
+            <Anonymous>익명</Anonymous>
+          </AnonymousBox>
+        </ButtonLabel>
+      </KeyboardAccessoryView>
+    </Container>
+  );
+};
 
 export default WritePostPresenter;
