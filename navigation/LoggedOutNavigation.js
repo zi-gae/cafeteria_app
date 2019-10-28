3f223fb193aa08ba943dd838967a980005fbcaab
devGW <doscmDev@gmail.com>
2019-10-28 18:02:36 +0900

    refactor: not used code remove

--- navigation/LoggedOutNavigation.js
+++ navigation/LoggedOutNavigation.js
@@ -1,25 +1,13 @@
 import { createAppContainer, createStackNavigator } from "react-navigation";
 import Transition from "../screens/Transition";
-import TakePost from "../screens/TakePost";
 
-const LoggedOutNavigation = createStackNavigator(
-  {
-    Tabs: {
-      screen: Transition,
-      navigationOptions: {
-        header: null
-      }
+const LoggedOutNavigation = createStackNavigator({
+  Tabs: {
+    screen: Transition,
+    navigationOptions: {
+      header: null
     }
-  },
-  {
-    TakePost: {
-      screen: TakePost,
-      navigationOptions: {
-        header: null
-      }
-    }
-  },
-  { mode: "modal" }
-);
+  }
+});
 
 export default createAppContainer(LoggedOutNavigation);
