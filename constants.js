21e68e74a302c0cc518461a3c09d3d72a581ee54
geonwoo.jeong <doscmDev@gmail.com>
2019-11-23 15:20:12 +0900

    Delete constants.js

--- constants.js
+++
@@ -1,6 +0,0 @@
-// export const URL = "http://4d342cdc.ngrok.io";
-// export const URL = "http://3.133.10.78:8000";
-export const URL = "http://localhost:8000";
-
-export const TOKEN =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxOCwidXNlcm5hbWUiOiJ0b2tlbiIsImV4cCI6MTU3NDE4NjIyNywiZW1haWwiOiJwdWJsaWNAcHVibGljLmNvbSJ9.lTybAtgb0Mn01NEqrNEw2WgkV7nL54UkcKxNgRFNtVw";
